7cf19db12da3a5074eb1873d33f914ca28c789b9
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-10 21:50:59 +0530

    How to install Numpy in our Pc.

--- numpy.py
+++ numpy.py
@@ -2,3 +2,9 @@
 #Python list are slow for large numerical data.
 #Numpy gives: 1. Fast computation C-backed,, 2.Multi dimensional arrays,,
 #3. Mathematical & Linear algebra power,, 4. Backbone of Pandas, Tensorflow,PyTorch.
+
+#If we want to use NumPy in our System then we have to first install Numpy in our System.
+#We have to first open our terminal then use some basic command to install.
+# 1. pip install numpy
+# 2. import numpy as np
+# We sucessfully installed NumPy in our System.
