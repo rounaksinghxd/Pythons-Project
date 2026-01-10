ce5f20afe4e672f5a9d8c2d5f324238efb3c6bc6
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-10 21:44:53 +0530

    Learn about the basics of NumPy

---
+++ numpy.py
@@ -0,0 +1,4 @@
+#In this code we learn about Numpy in Python.
+#Python list are slow for large numerical data.
+#Numpy gives: 1. Fast computation C-backed,, 2.Multi dimensional arrays,,
+#3. Mathematical & Linear algebra power,, 4. Backbone of Pandas, Tensorflow,PyTorch.
