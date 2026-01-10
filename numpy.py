55952e7415918985ef26c7a79311e3cc236bb05c
rounaksinghxd <singhrounak00326@gmail.com>
2026-01-10 22:00:25 +0530

    Correcting some Error

--- numpy.py
+++ numpy.py
@@ -1,10 +1,18 @@
 #In this code we learn about Numpy in Python.
-#Python list are slow for large numerical data.
-#Numpy gives: 1. Fast computation C-backed,, 2.Multi dimensional arrays,,
-#3. Mathematical & Linear algebra power,, 4. Backbone of Pandas, Tensorflow,PyTorch.
+"""Python list are slow for large numerical data.
+Numpy gives: 
+1. Fast computation C-backed.
+2.Multi dimensional arrays.
+3. Mathematical & Linear algebra power.
+4. Backbone of Pandas, Tensorflow,PyTorch.
 
-#If we want to use NumPy in our System then we have to first install Numpy in our System.
-#We have to first open our terminal then use some basic command to install.
-# 1. pip install numpy
-# 2. import numpy as np
-# We sucessfully installed NumPy in our System.
+If you want to use NumPy in your System then you have to first install Numpy in your System.
+You have to first open your terminal then use some basic commands to install it.
+1. pip install numpy
+2. import numpy as np
+
+And if you want to check that weather you successfully installed numpy or not then use:
+import numpy
+print(numpy.__version__)
+If something appears like: 2.4.0 or any kind of other decimal digits then it means NumPy successfully installed in your PC.
+"""
